63d576c28ee3af6c8732ea86d7155eed14e24a7d
Andrs <andres.bermudez@mass.com.co>
2023-02-28 15:40:21 -0500

    perf(snippets): :art: update snippets
    update, sección, doble columna y se instalo esbuild

---
+++ esbuild.js
@@ -0,0 +1,28 @@
+// file: esbuild.js
+
+import { build } from "esbuild";
+
+const baseConfig = {
+  bundle: true,
+  minify: process.env.NODE_ENV === "production",
+  sourcemap: process.env.NODE_ENV !== "production",
+};
+
+const extensionConfig = {
+  ...baseConfig,
+  platform: "node",
+  format: "cjs",
+  entryPoints: ["./src/extension.ts"],
+  outfile: "./out/extension.js",
+  external: ["vscode"],
+};
+
+(async () => {
+  try {
+    await build(extensionConfig);
+    console.log("build complete");
+  } catch (err) {
+    process.stderr.write(err.stderr);
+    process.exit(1);
+  }
+})();
